0d7ff025bf83dfed265e378404a76ec2dff2e37e
Milan Soragna <milan.soragna@gmail.com>
2023-03-10 19:00:24 +0100

    Initial commit

---
+++ intranet/view/documentation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../design/css/style.css" />
+    <link rel="stylesheet" href="../design/css/navbar.css" />
+    <title>Intranet Cipher - Documentation</title>
+</head>
+<body>
+<?php
+include(__DIR__ . "/navbar.viewpart.php");
+?>
+<div class="main-content">
+    <h1>Documentation</h1>
+    <h2>Table des matières</h2>
+    <ul>
+        <li><a>Installation de Zabbix</a></li>
+        <li><a>Installation de NFS</a></li>
+        <ul>
+            <li><a>Sur le serveur</a></li>
+            <li><a>Sur le client</a></li>
+        </ul>
+        <li><a>Installation des pare-feu</a></li>
+        <ul>
+            <li><a>Rèles générales d'entrée</a></li>
+            <li><a>Règles générales de sortie</a></li>
+            <li><a>Script de réinitialisation</a></li>
+        </ul>
+        
+    </ul>
+</div>
+</body>
+</html>
---
+++ intranet/view/navbar.viewpart.php
@@ -0,0 +1,8 @@
+<nav>
+    <div>Cipher</div>
+    <ul>
+        <li><a href="./view/documentation.php">Documentation</a></li>
+        <li><a href="">Ressources</a></li>
+        <li><a href="">Carte</a></li>
+    </ul>
+</nav>
