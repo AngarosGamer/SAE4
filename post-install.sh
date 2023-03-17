41188743e48a7d4d91d3eb22cc5dd7c92ba78362
Milan Soragna <milan.soragna@gmail.com>
2023-03-17 16:49:04 +0100

    Post-installation script for a machine

---
+++ post-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Vérifier si l'utilisateur est root
+if [ "$EUID" -ne 0 ]
+  then echo "Ce script doit être utilisé en tant que root pour avoir les permissions nécessaires d'installation"
+  exit
+fi
+
+read -rp 'Nom de la machine [défaut: cipher]:' hostname
+
+hostnamectl set-hostname "$hostname"
+
+read -srp 'Utilisateur par défaut: ' username
+
+adduser "$username"
+
+echo "Modification du mot de passe root. Choisissez un mot de passe suffisement complexe"
+
+passwd
