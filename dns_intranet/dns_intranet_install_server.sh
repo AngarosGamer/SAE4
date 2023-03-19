7fff46fd9dc84e8b6685812ac3f225dce343c13c
OKB43 <thomas.boussit@gmail.com>
2023-03-19 21:25:08 +0100

    creation fichiers dns et  separation dhcp.conf

---
+++ dns_intranet/dns_intranet_install_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être lancé en tant que root" 
+   exit 1
+fi
+
+#install bind9 packages
+
+apt install bind9
+
+#verify if bind9 is installed
+if ! which bind9 > /dev/null; then
+   echo -e "bind9 packages not installed, please advuise your system administrator".
+fi
+
