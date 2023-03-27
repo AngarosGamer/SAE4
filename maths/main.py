b225eae1a76c73cb9f63abab071414a7e5e61ed5
dunandau <aurele.dunand@etu.univ-grenoble-alpes.fr>
2023-03-27 13:38:35 +0200

    Comment for maths

--- maths/main.py
+++ maths/main.py
@@ -51,51 +51,51 @@ def soustraction_colonne(matrice):
     return matrice
 
 def encadrer_zeros(matrice):
-    zero_encadre_indexes = []
-    zero_barre_indexes = []
-    lcounter = 0
-    ccounter = 0
-    lerror = False
-    cerror = False
-    for ligne in matrice:
-        for colonne in ligne:
-            if (colonne == 0):
-                if (len(zero_encadre_indexes) == 0):
-                    zero_encadre_indexes.append([lcounter, ccounter])
-                else:
-                    for i in zero_encadre_indexes:
-                        if lcounter == i[0]:
-                            lerror = True
-                        if ccounter == i[1]:
-                            cerror = True
-                    if not lerror and not cerror:
+    zero_encadre_indexes = [] # Les zéros encadrés
+    zero_barre_indexes = [] # Les zéros barrés
+    lcounter = 0 # Compteur de ligne
+    ccounter = 0 # Compteur de colonne
+    lerror = False # Le zéro trouvé est-il sur une ligne où un zéro est encadré
+    cerror = False # Le zéro trouvé est-il sur une colonne où un zéro est encadré
+    for ligne in matrice: # Parcours des lignes
+        for colonne in ligne: # Pour chaque élément de la ligne (donc colonne)
+            if (colonne == 0): # Si l'élément est un zéro
+                if (len(zero_encadre_indexes) == 0): # Si c'est le premier zéro à être trouvé
+                    zero_encadre_indexes.append([lcounter, ccounter]) # Le zéro peut être directement encadré
+                else: # Pas le premier zéro à être trouvé
+                    for i in zero_encadre_indexes: # Pour chacun des zéros déjà encadrés
+                        if lcounter == i[0]: # Si le zéro actuel appartient à une ligne déjà encadrée
+                            lerror = True # Erreur, le zéro doit être barré
+                        if ccounter == i[1]: # Si le zéro actuel appartient à une colonne déjà encadrée
+                            cerror = True # Erreur, le zéro doit être barré
+                    if not lerror and not cerror: # Pas d'erreur, le zéro peut être encadré (il n'est pas sur une ligne où colonne ayant un zéro encadré)
                         zero_encadre_indexes.append([lcounter, ccounter])
-                    else:
+                    else: # Erreur, le zéro est sur une ligne ou colonne possédant un zéro déjà encadré
                         zero_barre_indexes.append([lcounter, ccounter])
-                    lerror = False
+                    lerror = False # Reset de variable
                     cerror = False
 
-            ccounter += 1
-        ccounter = 0
-        lcounter += 1
-    return zero_encadre_indexes, zero_barre_indexes
+            ccounter += 1 # Colonne suivante (élément suivant)
+        ccounter = 0 # Reset de variable
+        lcounter += 1 # Ligne suivante
+    return zero_encadre_indexes, zero_barre_indexes # Retour des positions des zéros encadrés et zéros barrés
 
 def marquer_lignes_sans_zero_encadre(matrice, indexes):
     lignes = []
     lignes_zero = []
-    for i in range(len(matrice)):
-        lignes.append(i)
-    for i in indexes:
-        lignes_zero.append(i[0])
-    return list(set(lignes).difference(lignes_zero))
+    for i in range(len(matrice)): # Pour chaque ligne dans la matrice
+        lignes.append(i) # On ajoute le numéro de ligne dans la liste des lignes
+    for i in indexes: # Pour chaque index de zéro encadré
+        lignes_zero.append(i[0]) # On rajoute la ligne de ce zéro encadré
+    return list(set(lignes).difference(lignes_zero)) # On retourne la différence entre la liste des lignes et la liste des lignes ayant un zéro encadré (soit les listes n'ayant pas de zéro encadré)
 
 def marquer_colonnes_avec_zero_barre_sur_ligne_marquee(indexes_barres, lignes_marques):
     colonne_marquees = []
-    for barre in indexes_barres:
-        for ligne in lignes_marques:
-            if barre[0] == ligne:
-                colonne_marquees.append(barre[1])
-    return colonne_marquees
+    for barre in indexes_barres: # Pour chaque zéro barré
+        for ligne in lignes_marques: # Pour chaque ligne déjà marquée
+            if barre[0] == ligne: # Si la ligne du zéro barré est égale à la ligne marquée, il faut marquer la colonne du zéro barré 
+                colonne_marquees.append(barre[1]) # Ajout colonne
+    return colonne_marquees # Return
 
 def marquer_lignes_avec_zero_encadre_sur_colonnes_marquee(indexes_encadres, colonnes_marques, lignes_marquees):
     for encadre in indexes_encadres:
