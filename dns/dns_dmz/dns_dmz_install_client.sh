f83f26bd40485c4ccf73ac8302e5c324fef18d18
boussitt <thomas.boussit@etu.univ-grenoble-alpes.fr>
2023-03-28 17:16:14 +0200

    modif dns server intra

--- dns/dns_dmz/dns_dmz_install_client.sh
+++ dns/dns_dmz/dns_dmz_install_client.sh
@@ -9,15 +9,37 @@
 resolvFile="/etc/resolv.conf"
 interfacesFile="/etc/network/interfaces"
 #modifier le fichier /etc/resolv.conf
-sed -i '$a\//adresse IP primaire du dns de la dmz' $resolvFile
-sed -i '$a\nameserver 192.168.0.4' $resolvFile
+    sed -i '$a\//adresse IP primaire du dns de la dmz' $resolvFile
+    sed -i '$a\nameserver 192.168.0.4' $resolvFile
+
+#vérifier les paramètres réseau
+    ip addr add 192.168.0.4 dev eth1
+    ip route add default via 192.168.0.4
 
 #ajouter le nom du serveur DNS dans /interfaces
 #verification si la ligne "iface eth0 inet static" est présente
-    if grep 'iface eth0 inet static' "$interfacesFile"; then
-    sed -i 'iface eth0 inet static/a    dns-nameservers 192.168.0.4' $interfacesFile
+    if grep 'auto eth1' "$interfacesFile"; then
+        if grep 'iface eth1 inet static' "$interfacesFile"; then
+        sed -i 'iface eth1 inet static/a    dns-nameservers 192.168.0.4' $interfacesFile
+        else
+        sed -i '$a\auto eth1\niface eth0 inet static\n    dns-nameservers 192.168.0.4' $interfacesFile
+        fi
     else
-    sed -i '$a\iface eth0 inet static\n    dns-nameservers 192.168.0.4' $interfacesFile
+        if grep 'iface eth1 inet static' "$interfacesFile"; then
+        #remplacer la ligne avant 'iface eth0 inet static' par 'auto eth1'
+        sed 'iface eth1 inet static/ i\auto eth1' $interfacesFile
+        sed -i 'iface eth1 inet static/a    dns-nameservers 192.168.0.4' $interfacesFile
+        #ajouter la ligne 'iface eth0 inet static' après 'auto eth1'
+        else
+        #ajouter à la fin du document les lignes suivantes
+        cat > $interface << 'EOF'
+        auto eth1
+        iface eth1 inet static
+        address 192.168.0.4
+        netmask 255.255.255.0
+        gateway 192.168.0.0
+EOF
+        fi
     fi
 
 #redémarrer le service network
--- dns/dns_intranet/dns_intranet_install_server.sh
+++ dns/dns_intranet/dns_intranet_install_server.sh
@@ -1,39 +1,80 @@
 #!/bin/bash
-#Check if the user is root
-if [[ $EUID -ne 0 ]]; then
-   echo "Ce script doit être lancé en tant que root" 
-   exit 1
-fi
+#Verfie si le script est lancé en tant que root
+    if [[ $EUID -ne 0 ]]; then
+    echo "Ce script doit être lancé en tant que root" 
+    exit 1
+    fi
 
-#install bind9 packages
+#installe les packages bind9
+    apt update
+    apt install bind9
 
-apt install bind9
-
-#verify if bind9 is installed
-if ! which bind9 > /dev/null; then
-   echo -e "bind9 packages not installed, please advise your system administrator".
-fi
-
-#ajouter le nom du serveur DNS dans /hostsname
-# cat > /etc/network/interfaces << 'EOL'
-   # This file contain the FQDN of the DNS server
- #  dns.cipher.intra
-
-#EOL
-
-#creer la zone de l'intranet
-cat > /etc/named.conf << 'EOL'
-// Define the local network zone
-zone "intra.cipher.com" {
-    type master;
-    file "/etc/bind/zones/intra.cipher.com";
-    allow-transfer { key "transfer-key"; };
-};
-
-// Define a reverse lookup zone for the local network
-zone "1.168.192.in-addr.arpa" {
-    type master;
-    file "/etc/bind/zones/db.192.168.1";
-    allow-transfer { key "transfer-key"; };
-};
+#verifie si bind9 est installé
+    if ! which bind9 > /dev/null; then
+    echo -e "bind9 packages not installed, please advise your system administrator".
+    fi
+#completer le fichier named.conf.local
+    cat > /etc/bind/named.conf.local << 'EOL'
+    zone "users.cipher" {
+        type master;
+        file "/etc/bind/db.users.cipher.";
+    };
+    zone "servers.cipher" {
+        type master;
+        file "/etc/bind/db.servers.cipher.";
+    };
 EOL
+
+fileNamedConf="/users/info/etu-2a/boussitt/SAE4/test.txt"
+fileNamedConfLocal="/users/info/etu-2a/boussitt/SAE4/a.conf.local"
+
+# fichier de zone pour les serveurs 
+    touch /etc/bind/db.servers.cipher.
+#creer un fichier de zone pour les machines de l'intranet
+    touch /etc/bind/db.users.cipher.
+
+#remplir le fichier db.servers.cipher.
+    cat > /etc/bind/db.servers.cipher. << 'EOL'
+    $TTL 86400
+    @       IN      SOA     servers.cipher.com. root.servers.cipher. (
+                            2020102001      ; Serial
+                            3600            ; Refresh
+                            1800            ; Retry
+                            604800          ; Expire
+                            86400 )         ; Minimum TTL
+    ;
+    @       IN      NS      servers.cipher.
+    sgbd      IN      A     192.168.1.3
+    log       IN      A     192.168.1.4
+    files     IN      A     192.168.1.5
+    dns       IN      A     192.168.1.6
+    ldap      IN      A     192.168.1.7
+    dhcp      IN      A     192.168.1.8
+    kerberos  IN      A     192.168.1.9
+
+EOL
+#remplir le fichier db.users.cipher.
+    cat > /etc/bind/db.users.cipher. << 'EOL'
+    $TTL 86400
+    @       IN      SOA     users.cipher.com. root.users.cipher. (
+                            2020102001      ; Serial
+                            3600            ; Refresh
+                            1800            ; Retry
+                            604800          ; Expire
+                            86400 )         ; Minimum TTL
+    ;
+    @       IN      NS      users.cipher.
+EOL
+
+#ajouter les directives au fichier interfaces
+    cat > /etc/network/interfaces << 'EOL'
+    iface eth1 inet static
+    address 192.168.1.6
+    netmask 255.255.255.0
+    dns-nameservers 192.168.1.6
+EOL
+
+#redemarrer les services 
+    systemctl restart bind9
+    systemctl restart networking
+    ifdown eth1 && ifup eth1
