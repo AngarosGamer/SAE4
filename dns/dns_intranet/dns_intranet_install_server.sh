7ada4fcde9b819121f66d64f3bb1d1cf4f5862a7
boussitt <thomas.boussit@etu.univ-grenoble-alpes.fr>
2023-03-29 17:08:36 +0200

    adaptation srcipt serv dns intra

--- dns/dns_intranet/dns_intranet_install_server.sh
+++ dns/dns_intranet/dns_intranet_install_server.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# Fonctions
+    function error {
+        >&2 echo -e "\033[1;31mERROR:\033[0m $1"; shift
+        exit "${1:-1}"
+    }
 #Verfie si le script est lancé en tant que root
     if [[ $EUID -ne 0 ]]; then
-    echo "Ce script doit être lancé en tant que root" 
-    exit 1
+        echo "Ce script doit être lancé en tant que root" 
+        exit 1
     fi
 
 #installe les packages bind9
-    apt update
-    apt -y install bind9
+    apt -y install bind9 || error "L'installation a échoué."
 
 #verifie si bind9 est installé
     if ! which bind9 > /dev/null; then
@@ -53,6 +57,7 @@ dhcp      IN      A     192.168.1.8
 kerberos  IN      A     192.168.1.9
 
 EOL
+echo "Fichier db.servers.cipher. créé"
 #remplir le fichier db.users.cipher.
     cat > /etc/bind/db.users.cipher. << 'EOL'
 $TTL 86400
@@ -65,7 +70,7 @@ $TTL 86400
 ;
 @       IN      NS      users.cipher.
 EOL
-
+echo "Fichier db.users.cipher. créé"
 #ajouter les directives au fichier interfaces
     cat > /etc/network/interfaces << 'EOL'
 iface enp1s0 inet static
@@ -73,7 +78,7 @@ address 192.168.1.6
 netmask 255.255.255.0
 dns-nameservers 192.168.1.6
 EOL
-
+echo "Fichier interfaces modifié"
 #redemarrer les services 
     systemctl restart bind9
     systemctl restart networking
