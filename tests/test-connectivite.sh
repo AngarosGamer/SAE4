c561b46866a9c6989f77408837e4aaa00909b47b
Milan Soragna <milan.soragna@gmail.com>
2023-04-01 18:11:39 +0200

    Small change to test-connectivité to include hash table

--- tests/test-connectivite.sh
+++ tests/test-connectivite.sh
@@ -3,12 +3,29 @@
 # Test de connectivite
 # Vérifie que les machines sont joignables par ping ICMP
 
-machines="192.168.0.2 192.168.0.3 192.168.0.4 192.168.1.2 192.168.1.3 192.168.1.4 192.168.1.5 192.168.1.6 192.168.1.7 192.168.1.8 192.168.1.9 192.168.1.10 192.168.2.2 192.168.2.3 192.168.2.4 192.168.2.5"
 
-for ip in $machines; do
+declare -A machines=(["192.168.0.2"]="dmz-routeur" 
+                    ["192.168.0.3"]="dmz-webserver"
+                    ["192.168.0.4"]="dmz-dns"
+                    ["192.168.1.2"]="serveur-routeur"
+                    ["192.168.1.3"]="serveur-postgres"
+                    ["192.168.1.4"]="serveur-log"
+                    ["192.168.1.5"]="serveur-fichiers"
+                    ["192.168.1.6"]="serveur-dns"
+                    ["192.168.1.7"]="serveur-ldap"
+                    ["192.168.1.8"]="serveur-dhcp"
+                    ["192.168.1.9"]="serveur-kerberos"
+                    ["192.168.1.10"]="serveur-zabbix"
+                    ["192.168.2.2"]="machines-routeur"
+                    ["192.168.2.3"]="machines-poste-3"
+                    ["192.168.2.4"]="machines-poste-4"
+                    ["192.168.2.5"]="machines-poste-5"
+)
+
+for ip in "${!machines[@]}"; do
     if ping -c 1 "$ip" > /dev/null; then
-        echo "$ip est joignable"
+        echo "$ip : ${machines[$i]} est joignable"
     else
-        echo "$ip n'est pas joignable"
+        echo "$ip : ${machines[$i]} n'est pas joignable"
     fi
 done
