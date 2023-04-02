2de43337a1e3d10b0d32a97e688cbfdf7b60ffc5
Aurele73 <127544459+Aurele73@users.noreply.github.com>
2023-04-02 12:59:02 +0200

    nfs & ldap
    nfs install client demande maintenant l'ip du serveur de fichier.

--- nfs/nfs_install_client.sh
+++ nfs/nfs_install_client.sh
@@ -5,12 +5,18 @@ if [ "$EUID" -ne 0 ]
   then echo "Ce script doit être utilisé en tant que root pour avoir les permissions nécessaires d'installation"
   exit
 fi
+if [[ "$#" -ne 1 ]];
+then
+    echo "Il faut renseigner l'adresse IP du serveur de fichier où NFS ira chercher les répertoire"
+    echo "Exemple de commande :"
+	echo "./nfs_install_client 192.168.1.5 "
+else
 
 # Installer le client NFS sur la machine
 apt -y install nfs-common
 
 # Monter le homedir dans le serveur NFS
-mount 192.168.128.1:/home /home
+mount $1:/home /home
 ls /home
 
-"192.168.128.4:/home   /home   nfs  noauto,x-systemd.automount   0  0" >> /etc/fstab
+echo "$1:/home   /home   nfs  noauto,x-systemd.automount   0  0" >> /etc/fstab
