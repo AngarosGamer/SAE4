bacf02b52edb3f4947e876acbff337bc39d5444d
Milan Soragna <milan.soragna@gmail.com>
2023-04-03 14:25:05 +0200

    Firewall reset

--- firewall/firewall_reset.sh
+++ firewall/firewall_reset.sh
@@ -6,11 +6,21 @@
 # Pour éviter de poser des problèmes, il est recommandé d'utiliser le script main_firewall.sh (après édition des règles) pour reconfigurer le firewall
 
 # Supprimer toutes les règles du pare-feu
-nft flush ruleset
+flush ruleset
 
 # Accepter toutes les connexions entrantes et sortantes
-nft add table ip filter
-nft add chain filter input { type filter hook input priority 0; policy accept; }
-nft add chain filter output{ type filter hook output priority 0; policy accept; }
-nft add chain filter forward { type filter hook forward priority 0; policy accept; }
-nft add chain filter postrouting { type filter nat postrouting priority 0; policy accept ; masquerade }
+table ip filter {
+    chain input {
+        type filter hook input priority 0; policy accept;
+    }
+    chain output{
+        type filter hook output priority 0; policy accept;
+    }
+    chain forward {
+        type filter hook forward priority 0; policy accept;
+    }
+    chain postrouting {
+        type filter nat postrouting priority 0; policy accept;
+        masquerade
+    }
+}
